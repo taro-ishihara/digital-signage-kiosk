ba7701f38b4bbe2f3d73c0fa4bf4a2bb8be3247d
Taro Ishihara <taro.ishihara.mail@gmail.com>
2023-03-29 22:55:57 -0700

    add video

--- src/components/ContentsManager.js
+++ src/components/ContentsManager.js
@@ -1,6 +1,7 @@
 import { useState, useReducer } from "react";
 
 import Image from "./Image.js";
+import Video from "./Video.js";
 import React from "react";
 import { useContents } from "../context/ContentsContext";
 
@@ -13,7 +14,7 @@ const ContentsManager = () => {
       case "index/next":
         if (contents.length < 2) {
           setForceFlipToggle(!forceFlipToggle);
-          return 0
+          return 0;
         }
         if (index < contents.length - 1) {
           return index + 1;
@@ -25,8 +26,16 @@ const ContentsManager = () => {
     }
   }, 0);
 
-  return (
+  return contents.length === 0 ? (
+    <img src="default.png" alt="" />
+  ) : contents[index].contentType.startsWith("image") ? (
     <Image
+      {...contents[index]}
+      dispatch={dispatch}
+      forceFlipToggle={forceFlipToggle}
+    />
+  ) : (
+    <Video
       {...contents[index]}
       dispatch={dispatch}
       forceFlipToggle={forceFlipToggle}
